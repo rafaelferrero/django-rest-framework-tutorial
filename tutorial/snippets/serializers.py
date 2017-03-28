f5462f3d1e58bc2e2ae89ef511b4148462c0bd73
Rafael Ferrero <rafael.ferrero@gmail.com>
2017-03-28 17:40:31 -0300

    fix import with trailing commas

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
-from tutorial.snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES,
+from tutorial.snippets.models import (
+    Snippet,
+    LANGUAGE_CHOICES,
+    STYLE_CHOICES,
+)
 
 
 class SnippetSerializer(serializers.Serializer):
